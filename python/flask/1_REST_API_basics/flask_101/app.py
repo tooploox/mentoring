6e5125c6e4d1ba34d21682afb93b880fd6100845
roman.koziolek <roman.koziolek@tooploox.com>
2020-06-18 00:22:24 +0200

    PythonTrack: Reverted the exercise stub for flask_101.

--- python/flask/1_REST_API_basics/flask_101/app.py
+++ python/flask/1_REST_API_basics/flask_101/app.py
@@ -1,28 +1,9 @@
-# """Flask tutorial 101
-#
-# Write your code below.
-# Details in the readme.md file.
-# """
-#
-#
-# if __name__ == '__main__':
-#     pass
+"""Flask tutorial 101
 
-"""Flask tutorial 101 - solution
-
+Write your code below.
 Details in the readme.md file.
 """
 
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def status():
-    return dict(status="ok")
-
-
 if __name__ == '__main__':
-    app.run()
+    pass
