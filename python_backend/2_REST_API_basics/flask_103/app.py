255df4fa76ed8c021a215bc4e453a773d884209e
roman.koziolek <roman.koziolek@tooploox.com>
2020-09-14 11:04:58 +0200

    FileUpload: Working code moved to solution dir.

--- python_backend/2_REST_API_basics/flask_103/app.py
+++ python_backend/2_REST_API_basics/flask_103/app.py
@@ -4,56 +4,14 @@ Write your code below.
 Details in the readme.md file.
 """
 
-import os
-
-from flask import Flask, request
-from PIL import Image
-from werkzeug.utils import secure_filename
+from flask import Flask
 
 app = Flask(__name__)
-
-app.config['UPLOAD_FOLDER'] = 'upload'
-app.config['MAX_CONTENT_LENGTH'] = 450_000
-
-
-def get_secure_filename(filename):
-    return os.path.join(app.config['UPLOAD_FOLDER'],
-                        secure_filename(filename))
 
 
 @app.route('/')
 def index():
     return {'status': 'ok'}
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        files = request.files.getlist('files')
-        response = {}
-        for file in files:
-            path = get_secure_filename(file.filename)
-            file.save(path)
-            data = {}
-            im = Image.open(path)
-            data['mode'] = im.mode
-            data['format'] = im.format
-            scale = float(request.values.get('scale'))
-            if scale:
-                im = im.resize(
-                    (int(im.size[0] * scale), int(im.size[1] * scale)),
-                    Image.ANTIALIAS)
-                resized_file_path = get_secure_filename(
-                    f'scaled_{file.filename}')
-                im.save(resized_file_path)
-                data['scale'] = scale
-                data['size'] = im.size
-
-            message = f'file {file.filename!r} uploaded successfully.'
-            response[file.filename] = dict(message=message, data=data)
-        return response
-    else:
-        return {'message': 'POST your files here'}
 
 
 if __name__ == '__main__':
--- python_backend/2_REST_API_basics/flask_103/solution/app.py
+++ python_backend/2_REST_API_basics/flask_103/solution/app.py
@@ -1,18 +1,60 @@
-"""Flask tutorial 101 - solution
+"""Flask tutorial 103 - solution
 
 Details in the readme.md file.
 """
 
 
-from flask import Flask
+import os
+
+from flask import Flask, request
+from PIL import Image
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = 'upload'
+app.config['MAX_CONTENT_LENGTH'] = 450_000
 
-@app.route("/")
-def status():
-    return dict(status="ok")
+
+def get_secure_filename(filename):
+    return os.path.join(app.config['UPLOAD_FOLDER'],
+                        secure_filename(filename))
+
+
+@app.route('/')
+def index():
+    return {'status': 'ok'}
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        files = request.files.getlist('files')
+        response = {}
+        for file in files:
+            path = get_secure_filename(file.filename)
+            file.save(path)
+            data = {}
+            im = Image.open(path)
+            data['mode'] = im.mode
+            data['format'] = im.format
+            scale = float(request.values.get('scale'))
+            if scale:
+                im = im.resize(
+                    (int(im.size[0] * scale), int(im.size[1] * scale)),
+                    Image.ANTIALIAS)
+                resized_file_path = get_secure_filename(
+                    f'scaled_{file.filename}')
+                im.save(resized_file_path)
+                data['scale'] = scale
+                data['size'] = im.size
+
+            message = f'file {file.filename!r} uploaded successfully.'
+            response[file.filename] = dict(message=message, data=data)
+        return response
+    else:
+        return {'message': 'POST your files here'}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
